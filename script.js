568a8296e5c35ca44472903669944772c983a793
Rendi <36185561+kobmol@users.noreply.github.com>
2025-12-27 19:02:44 +0700

    Update README.md

---
+++ script.js
@@ -0,0 +1,107 @@
+<script>
+            function dataInputan(){
+            var nama = document.getElementById("Nama").value;
+            var ttl = document.getElementById("ttl").value;
+            var gender = document.getElementById("gender").value;
+
+            document.getElementById("hasil").innerHTML =( `
+            <fieldset>
+              <legend> Personal Data </legend>
+                <table>
+                    <tr>
+                    <td width="20px"> Nama <td>
+                    <td> : <td>
+                    <td coolspan="2px"> ${nama}
+                    </tr>
+                </table>
+            </fieldset>)
+            <fieldset>
+              <legend> Personal Data </legend>
+                <table>
+                    <tr>
+                    <td width="20px"> Nama <td>
+                    <td> : <td>
+                    <td coolspan="2px"> ${nama}
+                    </tr>
+                </table>
+            </fieldset>
+            <fieldset>
+              <legend> Personal Data </legend>
+                <table>
+                    <tr>
+                    <td width="20px"> Nama <td>
+                    <td> : <td>
+                    <td coolspan="2px"> ${nama}
+                    </tr>
+                </table>
+            </fieldset>)
+            <fieldset>
+              <legend> Personal Data </legend>
+                <table>
+                    <tr>
+                    <td width="20px"> Nama <td>
+                    <td> : <td>
+                    <td coolspan="2px"> ${nama}
+                    </tr>
+                </table>
+            </fieldset><fieldset>
+              <legend> Personal Data </legend>
+                <table>
+                    <tr>
+                    <td width="20px"> Nama <td>
+                    <td> : <td>
+                    <td coolspan="2px"> ${nama}
+                    </tr>
+                </table>
+            </fieldset>)
+            <fieldset>
+              <legend> Personal Data </legend>
+                <table>
+                    <tr>
+                    <td width="20px"> Nama <td>
+                    <td> : <td>
+                    <td coolspan="2px"> ${nama}
+                    </tr>
+                </table>
+            </fieldset><fieldset>
+              <legend> Personal Data </legend>
+                <table>
+                    <tr>
+                    <td width="20px"> Nama <td>
+                    <td> : <td>
+                    <td coolspan="2px"> ${nama}
+                    </tr>
+                </table>
+            </fieldset>)
+            <fieldset>
+              <legend> Personal Data </legend>
+                <table>
+                    <tr>
+                    <td width="20px"> Nama <td>
+                    <td> : <td>
+                    <td coolspan="2px"> ${nama}
+                    </tr>
+                </table>
+            </fieldset><fieldset>
+              <legend> Personal Data </legend>
+                <table>
+                    <tr>
+                    <td width="20px"> Nama <td>
+                    <td> : <td>
+                    <td coolspan="2px"> ${nama}
+                    </tr>
+                </table>
+            </fieldset>)
+            <fieldset>
+              <legend> Personal Data </legend>
+                <table>
+                    <tr>
+                    <td width="20px"> Nama <td>
+                    <td> : <td>
+                    <td coolspan="2px"> ${nama}
+                    </tr>
+                </table>
+            </fieldset>
+             }
+
+        </script>
